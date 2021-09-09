4359c4e41d21d925597b28dd0b4c936d9a23a0a1
t0kar <81991067+t0kar@users.noreply.github.com>
2021-09-09 20:24:58 +0200

    Add pokemon.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-const helloWorld = require("./helloWorld");
+const helloWorld = require("./src/helloWorld");
+const printYellowPokemon = require("./src/pokemon");
 
 helloWorld.printHelloWorld();
+
+fetch("https://pokeapi.co/api/v2/pokemon-color/yellow/")
+  .then((response) => response.json())
+  .then((json) => {
+    printYellowPokemon(json.pokemon_species);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
